ffc0be2ceaeb59619094089d22d127bb9a49fe07
Yazz <itsjustmaitai@gmail.com>
2025-07-20 16:53:16 +0300

    Created the category models and the product model

---
+++ server/models/category.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text)
+    image_url = db.Column(db.String(255))  # store the URL or path to image
+
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
